c736f7ebfce4a5d7bebc5a5f2b08d4c10392e3f8
farmt <farmt@DESKTOP-AR3SR7U>
2020-03-11 16:28:48 -0500

    Added Project to repo

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using DependencyInjection.Models;
+using System.Web.Mvc;
+using Ninject;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator calc;
+        private Product[] products =
+        {
+           new Product {Name = "Kayak", Category="Watersports", Price=275M },
+           new Product {Name = "Lifejacket",Category="Watersports", Price = 48.95M },
+           new Product {Name = "Soccer Ball", Category="Soccer", Price=19.50m },
+           new Product {Name = "Corner Flag", Category="Soccer", Price=34.95M },
+
+        };
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
---
+++ DependencyInjection/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Ninject;
+using DependencyInjection.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValuesCalculator>();
+        }
+    }
+}
